204a6f9d152c09d61bfc9e7e37ce8d5cedec9320
Michael-Barrionuevo <mvbarrionuevos.uce.edu.ec>
2026-02-05 18:43:53 -0500

    Taller 36

--- src/router/index.js
+++ src/router/index.js
@@ -17,43 +17,80 @@ const routes = [
   {
     path: '/consultar',
     name: 'ConsultarTodos',
-    component: () => import('../views/TodosView.vue')
+    component: () => import('../views/TodosView.vue'),
+    meta:{
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/buscar',
     name: 'ConsultarPorId',
-    component: () => import('../views/PorIdView.vue')
+    component: () => import('../views/PorIdView.vue'),
+    meta:{
+      requiereAutorizacion:false,
+      esPublica: true
+    }
   },
   {
     path: '/nuevo',
     name: 'Guardar',
-    component: () => import('../views/GuardarView.vue')
+    component: () => import('../views/GuardarView.vue'),
+    meta:{
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/editar',
     name: 'Actualizar',
     component: () => import('../views/ActualizarView.vue'),
-    props: true 
+    props: true ,
+    meta:{
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
 
   {
     path: '/editarparcial',
     name: 'ActualizarParcial',
     component: () => import('../views/ActualParcialView.vue'),
-    props: true 
+    props: true ,
+    meta:{
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
 
   {
     path: '/borrar',
     name: 'Borrar',
     component: () => import('../views/BorrarView.vue'),
-    props: true 
+    props: true ,
+    meta:{
+      requiereAutorizacion: false,
+      esPublica: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+/* Configuracion del Guardian */
+
+router.beforeEach((to,from,next)=>{
+  if(to.meta.requiereAutorizacion){
+    /* le envio a una pagina de login */
+    console.log("Redirigiendo a LOGIN")
+  }else{
+    /* le dejo sin validaciones */
+    console.log("Pase Libre")
+    next();
+  }
 })
 
 export default router
